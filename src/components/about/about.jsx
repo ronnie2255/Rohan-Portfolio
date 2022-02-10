c653f480c605db12e91151ab636f9bc78fc3e442
rohan <ronnie2255@icloud.com>
2022-02-10 18:54:15 -0500

    intro page header change

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -5,7 +5,7 @@ import "./about.scss";
 export default function About() {
   return ( 
   <div className="about" id="about">
-    <h1>Little about myself</h1>
+    <h1>About</h1>
         <div className="container">
           <div className="wrapper">
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur corrupti facere excepturi, blanditiis ipsam a sapiente culpa deleniti necessitatibus nihil rem exercitationem earum omnis? Quo voluptatem commodi perferendis dignissimos sint!</p>
