4fadd6fbea95a68bf7513c6cc1380f7d19fa4822
rohan <ronnie2255@icloud.com>
2022-03-01 11:48:36 -0500

    added exclamation

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -11,7 +11,7 @@ export default function Intro() {
             showCursor: true, 
             backDelay: 1500,
             backSpeed: 60,
-            strings: ["Bonjour!, Je suis", "Hi There, I'm" ],
+            strings: ["Bonjour!, Je suis", "Hi There!, I'm" ],
          });
     
     }, [])
