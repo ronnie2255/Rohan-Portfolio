8335623d877bbcc46db0e9502fefa43f0def5180
rohan <ronnie2255@icloud.com>
2022-03-01 08:42:41 -0500

    png images

--- src/data.js
+++ src/data.js
@@ -3,33 +3,39 @@ export const completedPortfolio = [
       id: 1,
       title: "Calculator App",
       img:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+        "https://i.ibb.co/Fb6mRWF/calculator.png",
     },
     {
       id: 2,
       title: "To-do list",
       img:
-        "https://cdn.dribbble.com/users/702789/screenshots/15054318/media/4ea5d492b7b07eebc9528ff960794879.png?compress=1&resize=1200x900",
+        "https://i.ibb.co/RjGdkxw/Todolist.png",
     },
     {
       id: 3,
       title: "E-commerce Website",
       img:
-        "https://cdn.dribbble.com/users/1387827/screenshots/15466426/media/deb2dca6762cd3610321c98bfccb0b72.png?compress=1&resize=1200x900",
+        "https://i.ibb.co/CQYHXqM/ecommerce.png",
     },
     {
       id: 4,
       title: "Login & Registration",
       img:
-        "https://cdn.dribbble.com/users/4095861/screenshots/15467417/media/d6a15c416626f12b31fa5ca1db192572.png?compress=1&resize=1200x900",
+        "https://i.ibb.co/L6XXxZ1/login-registration.png",
     },
     {
       id: 5,
       title: "Routing App",
       img:
-        "https://cdn.dribbble.com/users/5031392/screenshots/15467520/media/c36b3b15b25b1e190d081abdbbf947cf.png?compress=1&resize=1200x900",
+        "https://i.ibb.co/bNc2kj9/matrix-routing.png",
     },
   ];
+
+
+
+
+
+
   
   export const upcomingPortfolio = [
     {
