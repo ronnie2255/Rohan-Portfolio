c760e1af896759da32259fdbd02872e1afb485a7
rohan <ronnie2255@icloud.com>
2022-03-01 11:14:38 -0500

    About page is updated

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -8,11 +8,20 @@ export default function About() {
     <h1>About</h1>
         <div className="container">
           <div className="wrapper">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur corrupti facere excepturi, blanditiis ipsam a sapiente culpa deleniti necessitatibus nihil rem exercitationem earum omnis? Quo voluptatem commodi perferendis dignissimos sint!</p>
+          <p>Welcome to my profile,
+            <br /> 
+            <br />Previously a Research Analyst with 2+ years of experience working on projects from wide range of industries some of which are technology, retail and social sciences.  
+            </p>
 
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam quas ipsa explicabo distinctio doloremque sint quaerat veniam nihil laborum, voluptatibus vitae blanditiis voluptate, necessitatibus dignissimos iure, accusamus eligendi similique amet?</p>
+            <p>During my time as a research analyst I got the opportunity to analyze our client's websites and derive insights to improve their customers web experience. This compelled me to learn about web-development technologies to understand the Why?, How? and What? of web development. This was enough to lead me onto a self-taught journey into the world of Front-end web development. </p>
+
+            <p>My journey of becoming a self-taught Front-end developer has been extremely rewarding and wouldn't have been possible without the amazing resources available in the web-dev community.</p>
             
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam quas ipsa explicabo distinctio doloremque sint quaerat veniam nihil laborum, voluptatibus vitae blanditiis voluptate, necessitatibus dignissimos iure, accusamus eligendi similique amet?</p>
+            <p>What began as a task in my job, has become a fully blown passion into creating elegant, accessible, secure and customer centric websites. </p>
+
+            
+            
+            
           </div>
           
         </div>
