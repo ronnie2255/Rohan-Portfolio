ccccadbccc661a299ae83e0eb4a3546b1c0a905b
rohan <ronnie2255@icloud.com>
2022-03-21 15:02:33 -0400

    menu links adjusted

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -7,18 +7,18 @@ export default function Menu({ menuOpen, setMenuOpen }) {
                 <li onClick={()=> setMenuOpen(false)}>
                     <a href="#intro">Home</a>
                 </li>
-                <li onClick={()=> setMenuOpen(false)}>
+                {/* <li onClick={()=> setMenuOpen(false)}>
                     <a href="#about">About</a>
-                </li>
+                </li> */}
                 <li onClick={()=> setMenuOpen(false)}>
                     <a href="#portfolio">Portfolio</a>
                 </li >
                 <li onClick={()=> setMenuOpen(false)}>
                     <a href="#skills">Skills</a>
                 </li>
-                <li onClick={()=> setMenuOpen(false)}>
+                {/* <li onClick={()=> setMenuOpen(false)}>
                     <a href="#contact">Contact</a>
-                </li>
+                </li> */}
                 
             </ul> 
             
